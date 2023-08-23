8681f1bb90f43b6c0ced251b467dc14a6d9c97df
ESTELA ALVES DE MORAES <23111781@CFP127ALUNO.LOCAL>
2023-08-23 11:30:18 -0300

    versão 2 (switch case)

--- app.js
+++ app.js
@@ -8,8 +8,8 @@
 var readline = require('readline')
 
 var entradaDados = readline.createInterface({
-   input: process.stdin ,
-   output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 })
 
 console.log('')
@@ -19,69 +19,104 @@ console.log('+-----------')
 console.log('')
 console.log('Digite os valores: ')
 
-entradaDados.question('Valor 1: ', function (valorUm){
+entradaDados.question('Valor 1: ', function (valorUm) {
 
     let valor1 = valorUm.replace(',', '.')
 
-    if(valor1 == '' || isNaN(valor1)){
+    if (valor1 == '' || isNaN(valor1)) {
         console.log('>> ERRO: O valor deve ser um número.')
         entradaDados.close()
-    } else{
+    } else {
 
-        entradaDados.question('Valor 2: ', function(valorDois){
+        entradaDados.question('Valor 2: ', function (valorDois) {
 
             let valor2 = valorDois.replace(',', '.')
-            if(valor2 == '' || isNaN(valor2)){
+            if (valor2 == '' || isNaN(valor2)) {
                 console.log('>> ERRO: O valor deve ser um número.')
                 entradaDados.close()
-            }else{
-            console.log('')
-            console.log('1) Soma | 2) Subtração | 3) Multiplicação | 4) Divisão ')
-        entradaDados.question ('Escolha o número correspondente à operação: ', function(operacoes){
-    
-            let operacao = operacoes
-            let sinal
-            let resultado
+            } else {
+                console.log('')
+                console.log('1) Soma | 2) Subtração | 3) Multiplicação | 4) Divisão ')
+                entradaDados.question('Escolha o número correspondente à operação: ', function (operacoes) {
 
-            valor1 = Number(valor1)
-            valor2 = Number(valor2)
-            if(operacao == 1){
-                resultado = valor1+valor2
-                sinal = '+'
-            }else if(operacao == 2){
-                resultado = valor1-valor2
-                sinal = '-'
-            }else if(operacao == 3){
-                resultado = valor1*valor2
-                sinal = '*'
-            }else if(operacao == 4){
-                resultado = valor1/valor2
-                sinal = '/'
-            }
-            
-            if( operacao != 1 && operacao != 2 && operacao != 3 && operacao != 4) {
-                console.log('>> ERRO: Isso não corresponde à nenhuma operação.')
-                entradaDados.close()
-        }else if(operacao == 4 && valor2 == 0){
-            console.log('>. Erro: Não é possível dividir por 0')
-            entradaDados.close()
-        }else{
+                    let operacao = operacoes
+                    let sinal
+                    let resultado
 
-            entradaDados.question('Quantidade de casas decimais no resultado: ', function(casasDecimais){
+                    valor1 = Number(valor1)
+                    valor2 = Number(valor2)
 
-                casas = casasDecimais
-                console.log( String(valor1).replace('.', ',') + ' ' + sinal + ' ' + String(valor2).replace('.', ',') + '  =  ' + String(resultado.toFixed(casas)).replace('.', ','))
-                entradaDados.close()
-            })
+                    //     if(operacao == 1){
+                    //         resultado = valor1+valor2
+                    //         sinal = '+'
+                    //     }else if(operacao == 2){
+                    //         resultado = valor1-valor2
+                    //         sinal = '-'
+                    //     }else if(operacao == 3){
+                    //         resultado = valor1*valor2
+                    //         sinal = '*'
+                    //     }else if(operacao == 4){
+                    //         resultado = valor1/valor2
+                    //         sinal = '/'
+                    //     } 
+                    //     if( operacao != 1 && operacao != 2 && operacao != 3 && operacao != 4) {
+                    //         console.log('>> ERRO: Isso não corresponde à nenhuma operação.')
+                    //         entradaDados.close()
+                    // }else if(operacao == 4 && valor2 == 0){
+                    //     console.log('>. Erro: Não é possível dividir por 0')
+                    //     entradaDados.close()
+                    // }else{
+                    //      entradaDados.question('Quantidade de casas decimais no resultado: ', function(casasDecimais){
+                    //         casas = casasDecimais
+                    //         console.log( String(valor1).replace('.', ',') + ' ' + sinal + ' ' + String(valor2).replace('.', ',') + '  =  ' + String(resultado.toFixed(casas)).replace('.', ','))
+                    //         entradaDados.close()
+                    //     })
 
+                    //Versão 2.0 Switch
+                    switch (operacao) {
+                        case '1':
+                            resultado = valor1 + valor2
+                            sinal = '+'
+                            break
+                        case '2':
+                            resultado = valor1 - valor2
+                            sinal = '-'
+                            break
+                        case '3':
+                            resultado = valor1 * valor2
+                            sinal = '*'
+                            break
+                        case '4':
+                            if (operacao == 4 && valor2 == 0) {
+                                console.log('>. Erro: Não é possível dividir por 0')
+                                entradaDados.close()
+                            } else {
+                                resultado = valor1 / valor2
+                                sinal = '/'
+                            }
+                            break
 
+                        default:
+                            console.log('>> ERRO: Isso não corresponde à nenhuma operação.')
+                            entradaDados.close()
+                            break
+                    }
+
+                    if (sinal != undefined) {
+                        entradaDados.question('Quantidade de casas decimais no resultado: ', function (casasDecimais) {
+                            casas = casasDecimais
+                            console.log(String(valor1).replace('.', ',') + ' ' + sinal + ' ' + String(valor2).replace('.', ',') + '  =  ' + String(resultado.toFixed(casas)).replace('.', ','))
+                            entradaDados.close()
+                        })
         }
-            
 
-        })
-    }
+            })
+        }
 
     })
-            }
-
+}
 })
+
+
+
+
